4616a7e6204375c1e02974f352e78a4ff68f0622
luke bullock <GlimGrold2001@gmail.com>
2022-10-18 17:03:07 -0600

    Final, final, final changes

--- luke/js/writing.js
+++ luke/js/writing.js
@@ -4,7 +4,7 @@ var slider_img = document.querySelector("#one");
 var images = ["one.png", "two.png", "three.png", "four.png", "five.png", "six.png", "seven.png", "eight.png", 
 "nine.png", "ten.png", "eleven.png", "twelve.png", "thirteen.png", "fourteen.png", "fifteen.png", "sixteen.png",
 "seventeen.png", "eighteen.png", "nineteen.png", "twenty.png", "twenty_one.png", "twenty_three.png", 
-"twenty_four.png", "twenty_five.png", "twenty_six.png", "twenty_eight.png", "twenty_nine.png",
+"twenty_four.png", "twenty_five.png", "twenty_six.PNG", "twenty_eight.png", "twenty_nine.png",
 "thirty.png", "thirty_one.png", "thirty_two.png", "thirty_three.png", "thirty_four.png", "thirty_five.png"]; // I suppose Ill ad more later
 var i = 0; // Current page Index
 
